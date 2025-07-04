389fe55102fb41f919b30ae31295357ef705e9bb
Manisha Kumari <manishasinghrajput064@gmail.com>
2025-07-05 00:33:48 +0530

    modified

--- sensor_model.py
+++ sensor_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from flask import Flask
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
